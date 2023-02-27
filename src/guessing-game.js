5e886ffd8d4c4a90da01e185b1eb19c3632e61ad
Dmitry Kuzmik <kuzmikd@mail.ru>
2023-02-27 12:05:40 +0300

    feat:add guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.resultNumbers = Math.round((this.min + this.max)/2);
+        return this.resultNumbers;
     }
 
     lower() {
-
+        this.max = this.resultNumbers;
     }
 
     greater() {
-
+        this.min = this.resultNumbers;
     }
 }
 
